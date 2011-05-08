2f5efd589b81f68f54805d8818c7710ebfb1515e
unknown <Adron Hall@.(none)>
2011-05-08 12:17:33 -0700

    First executable and testable commit.

--- src/Regiztry.Tests/Class1.cs
+++ src/Regiztry.Tests/Class1.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using NUnit.Framework;
+using Regiztry.Controllers;
 
 namespace Regiztry.Tests
 {
-    public class Class1
+    [TestFixture]
+    public class HomeControllerTests
     {
+        [Test]
+        public void should_display_home_page()
+        {
+            var controller = new HomeController();
+            Assert.IsNotNull(controller.Index());
+        }
     }
 }
