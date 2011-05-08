46212996d2c38f41f5a28c5b1ae06e7e4cb7e1e0
unknown <Adron Hall@.(none)>
2011-05-07 17:26:37 -0700

    new stuff

--- Web/Web.Tests/Controllers/HomeController.cs
+++ Web/Web.Tests/Controllers/HomeController.cs
@@ -18,5 +18,21 @@ namespace Web.Tests.Controllers
             Assert.True(vResult != null, "controller result should be  a ViewResult");
             Assert.True(vResult.ViewName == string.Empty || vResult.ViewName == "Index", "view result should point to Index, but points to " + vResult.ViewName);
         }
+
+        [Test]
+        public void home_should_show_contact_information_page()
+        {
+            var controller = new HomeController();
+            var result = controller.Contact();
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void home_should_show_the_main_site_home_page()
+        {
+            var controller = new HomeController();
+            var result = controller.Index();
+            Assert.IsNotNull(result);
+        }
     }
 }
