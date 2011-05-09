22a445ae0d08768eda675cce48a45c764ac07a55
Adron Hall <adronhall@gmail.com>
2011-05-08 20:54:11 -0700

    Added first UI elements, CSS, javascript/jQuery UI, etc.

--- src/Regiztry/Models/Startup.cs
+++ src/Regiztry/Models/Startup.cs
@@ -5,7 +5,6 @@ namespace Regiztry.Models
     public class Startup
     {
         public int Id { get; set; }
-       
         public string Name { get; set; }
         public Uri WebSite { get; set; }
         public string Description { get; set; }
