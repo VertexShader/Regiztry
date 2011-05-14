84aad6621c01cecf0ead577cb4aed969699b59d8
ang3lfir3 <ang3lfir3@gmail.com>
2011-05-13 17:18:08 -0700

    getting the Mercahndise conntroller working... removing unneeded ID property from model

--- src/Regiztry/Models/Startup.cs
+++ src/Regiztry/Models/Startup.cs
@@ -5,7 +5,6 @@ namespace Regiztry.Models
     public class Startup
     {
         public Guid SisoId { get; set; }
-        public int Id { get; set; }
         public string Name { get; set; }
         public Uri WebSite { get; set; }
         public string Description { get; set; }
