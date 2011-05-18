ebfe84d95408ff7f048aa8ed4f557a8310c9822a
ang3lfir3 <ang3lfir3@gmail.com>
2011-05-18 16:45:45 -0700

    about to freak people out with the NSpec infection....mind.blown().should_be_true()

--- src/Regiztry.Tests/HomeControllerTests.cs
+++ src/Regiztry.Tests/HomeControllerTests.cs
@@ -1,23 +1,57 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NSpec;
+using NSpec.Domain;
+using NUnit.Framework;
 using Regiztry.Controllers;
 
 namespace Regiztry.Tests
 {
+    //Run the Runnershim with TD.Net to execute all the NSpec tests with NUnit.
     [TestFixture]
-    public class HomeControllerTests
+    public class RunnerShim
     {
         [Test]
-        public void should_display_home_page()
+        public void AllTests()
         {
-            var controller = new HomeController();
-            Assert.IsNotNull(controller.Show());
+            var finder = new ShimFinder(typeof(RunnerShim).Assembly.GetTypes());
+
+            var builder = new ContextBuilder(finder, new DefaultConventions());
+
+            var runner = new ContextRunner(builder);
+
+            runner.Run();
+
+            if (runner.Failures().Count() != 0)
+                Assert.Fail("Some NSpec test failed");
+        }
+    }
+
+    public class ShimFinder : ISpecFinder
+    {
+        private Type[] types;
+
+        public ShimFinder(Type[] types)
+        {
+            this.types = types;
         }
 
-        [Test]
-        public void should_display_contacts_page()
+        public IEnumerable<Type> SpecClasses()
         {
-            var controller = new HomeController();
-            Assert.IsNotNull(controller.Contact());
+            return types;
+        }
+    }
+
+
+    public class using_the_home_controller : nspec
+    {
+        HomeController controller = new HomeController();
+
+        public void when_calling_actions_on_the_controller()
+        {
+            specify = () => controller.Show().should_not_be_null();
+            specify = () => controller.Contact().should_not_be_null();
         }
     }
 }
