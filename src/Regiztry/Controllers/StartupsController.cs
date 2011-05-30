eb679a7d00bb5332f654057f139f3271a00defb9
Scott Koon <scott@lazycoder.com>
2011-05-30 00:01:01 -0700

    refactored controllers to use delegate so Eric can have his static methods. ;) Change the NSpec runnershim to enumerate over the failures and output the name and exception for each failure. Still kind of freaky that most test runners will report back all of the specs as '1 test'

--- src/Regiztry/Controllers/StartupsController.cs
+++ src/Regiztry/Controllers/StartupsController.cs
@@ -1,38 +1,40 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
 using Regiztry.Models;
+using System.Collections.Generic;
 
 namespace Regiztry.Controllers
 {
-    public class StartupsController : Controller
-    {
-        public ActionResult Show()
-        {
-            var startups = Regiztry.QueryWith(query => query.GetAll<Startup>().ToList());
+    public class StartupsController : RegiztryBaseController {
+
+        public QueryDelegate<IList<Startup>> QueryAll;
+        public GenericWorkOnDelegate<Startup> WorkOnStartup;
+
+        public StartupsController() {
+            QueryAll = Regiztry.GetQueryDelegate<IList<Startup>>();
+            WorkOnStartup = Regiztry.GetGenericWorkOnDelegate<Startup>();
+        }
+                                                                    
+        public ActionResult Show() {
+            var startups = QueryAll(query => query.GetAll<Startup>().ToList());
             return View(startups);
         }
 
-        public ActionResult Create()
-        {
+        public ActionResult Create() {
             return View(new Startup());
         }
 
         [HttpPost]
-        public ActionResult Create(Startup startup)
-        {
+        public ActionResult Create(Startup startup) {
             startup.Active = true;
 
-            try
-            {
-                Regiztry.WorkOn(work =>
-                {
+            try {
+                workOnDelegate(work => {
                     work.Insert(startup);
                     work.Commit();
                 });
                 return RedirectToAction("Show");
-            }
-            catch
-            {
+            } catch {
                 return View();
             }
         }
