a492c20a35bb031182d72166cde3de59e9851f99
Vertex Shader <bizadron@gmail.com>
2011-06-12 20:08:17 -0700

    Added prototype screens to start wire up of the settings and auth screens based around OpenID, Twitter, and Facebook.

---
+++ src/Regiztry/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Regiztry.Models;
+
+namespace Regiztry.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Settings()
+        {
+            var model = new Setting();
+
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Regiztry/Models/Setting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Regiztry.Models
+{
+    public class Setting
+    {
+        public string RegiztryUser { get; set; }
+        public string First { get; set; }
+        public string Last { get; set; }
+        public List<string> Groups { get; set; } // Founder, Contributer, User/Observer
+        public DateTime FirstIdentificationMade { get; set; }
+    }
+}
