9bcdc2953d02923734d7b0b445a7ca7d9c6d2755
Vertex Shader <bizadron@gmail.com>
2011-06-14 18:30:10 -0700

    Added a page to list the individuals that have helped build the Regiztry Site & Community.

---
+++ src/Regiztry/Models/Individuals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Regiztry.Models
+{
+    public class Individuals
+    {
+        public int Id{ get; set; }
+        public string Name { get; set; }
+        public List< Uri> WebPresence { get; set; }
+        public string About { get; set; }
+        public DateTime StartedHelping { get; set; }
+    }
+}
